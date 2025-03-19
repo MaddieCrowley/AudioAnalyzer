d833103d1df158ad4dd61023d3ef1d036122c1a1
Madeline Crowley <62516277+MaddieCrowley@users.noreply.github.com>
2025-03-18 23:02:28 -0400

    Made work with multiple buffer sizes at once

--- src/main.cpp
+++ src/main.cpp
@@ -11,20 +11,27 @@
 #elifdef X11
 #include "gui/xwin.h"
 #endif
-
+#define SMALL_BUFFER 10
 audio a;
-gui winXY(gui::L, 400, 400,
-          "L Fast",read::data,AUDIO_SIZE);
-gui winL(gui::L, 800, 600,
-    "L medium",read::data,AUDIO_SIZE*BUFFER_SIZE/4);
-gui winLong(gui::L, 800, 600,
-    "L slow",read::data,AUDIO_SIZE*BUFFER_SIZE);
+gui winXY(gui::XY, 400, 400,
+          "XY 1 Frame",read::data,AUDIO_SIZE);
+gui winL(gui::L, 1920, 600,
+         "L 4 Frames",read::data,AUDIO_SIZE*SMALL_BUFFER);
+gui winR(gui::R, 1920, 600,
+         "R 4 Frames",read::data,AUDIO_SIZE*SMALL_BUFFER);
+gui winLong(gui::XY, 600, 600,
+            "Full buffer",read::data,AUDIO_SIZE*BUFFER_SIZE);
 int main (int argc, char *argv[]) {
     while (!winXY.gDone) {
         winXY.loop();
         winXY.changeDataPtr(read::data+(AUDIO_SIZE*read::frameNum));
         winL.loop();
-        winL.changeDataPtr(read::data+(AUDIO_SIZE*read::frameNum/4));//TODO weird behavior (due to not finishing going through points before buffer updates?)
+        winR.loop();
+        if (read::frameNum%(SMALL_BUFFER)==0&&read::frameNum!=0) {
+            //std::cout << read::frameNum << std::endl;
+            winL.changeDataPtr(read::data+(AUDIO_SIZE*(read::frameNum-SMALL_BUFFER)));
+            winR.changeDataPtr(read::data+(AUDIO_SIZE*(read::frameNum-SMALL_BUFFER)));
+        }
         winLong.loop();
     }
     return 0;
